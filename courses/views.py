b9c429888e362a8d8f9749dfa720074384016a56
alex-artamonov <alex.artamonov2010@gmail.com>
2023-12-08 16:20:55 +0100

    add mixins to courses/views

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,28 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
+from .models import Course
 
-# Create your views here.
+class OwnerMixin:
+    def get_queryset(self) -> QuerySet[Any]:
+        qs =  super().get_queryset()
+        return qs.filter(owner=self.request.user)
+
+class OwnerEditMixin:
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+    
+class OwnerCourseMixin(OwnerMixin):
+    model = Course
+    fields = ['subjec', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+
+class ManageCourseListView(OwnerMixin, ListView):
+    template_name = 'courses/manage/course/list.html'
+    
+
+    
