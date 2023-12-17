dfb07ae01e946f92e9eced6bbb07c09f9b094847
alex-artamonov <alex.artamonov2010@gmail.com>
2023-12-17 22:42:30 +0100

    Add order functionality to content_list.html

--- courses/views.py
+++ courses/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, \
 from django.urls import reverse_lazy
 from django.apps import apps
 from django.forms.models import modelform_factory
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from .forms import ModuleFormSet
 from .models import Course, Module, Content
 
@@ -149,3 +150,23 @@ class ModuleContentListView(TemplateResponseMixin, View):
                                    id=module_id,
                                    course__owner=request.user)
         return self.render_to_response({'module': module})
+
+class ModelOrderView(CsrfExemptMixin,
+                     JsonRequestResponseMixin,
+                     View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id,
+                course__owner = request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+    
+
+class ContentOrderView(CsrfExemptMixin,
+                     JsonRequestResponseMixin,
+                     View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,
+                            module__course__owner = request.user)\
+                            .update(order=order)
+        return self.render_json_response({'saved': 'OK'})
