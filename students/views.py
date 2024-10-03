f5f18014de8e82447eebd4fb4b7342aec2e4aa77
alex-artamonov <alex.artamonov2010@gmail.com>
2024-10-03 14:55:24 +0200

    Find a bug in registration - extra comma in "authenticate"

--- students/views.py
+++ students/views.py
@@ -9,19 +9,18 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import CourseEnrollForm
 from courses.models import Course
-# Create your views here.
 
 class StudentRegistrationView(CreateView):
     template_name = 'students/student/registration.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('student_course_list')
 
-    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+    def form_valid(self, form):
         result =  super().form_valid(form)
         cd = form.cleaned_data
         user = authenticate(user=cd['username'],
-                            password=cd['password1'],)
-        login(self.request.user)
+                            password=cd['password1'])
+        login(self.request, user)
         return result
     
 class StudentEnrollCourseView(LoginRequiredMixin, FormView):
